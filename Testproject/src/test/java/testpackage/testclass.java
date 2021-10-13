df48daf3156260e90c081eb23467b4a9a3f7b231
Keerthanakulla <keerthanakulla06@gmail.com>
2021-10-13 10:53:20 -0400

    Update testclass.java
    removed unwanted print stmts

--- Testproject/src/test/java/testpackage/testclass.java
+++ Testproject/src/test/java/testpackage/testclass.java
@@ -3,10 +3,7 @@ package testpackage;
 public class testclass {
 	
 	public static void main(String args[]) {
-		System.out.println("hi I am a tester");
-		System.out.println("helloooooooooooo");
-		System.out.println("heeelllll2232323");
-		System.out.println("I am just a print");
+		System.out.println("Welcome to Canada");
 	}
 
 }
